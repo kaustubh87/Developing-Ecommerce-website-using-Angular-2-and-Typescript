a802269cf0d7c20509d7a704e307a8114cad58c4
kvin1087 <kaustubh87@gmail.com>
2018-01-17 15:52:24 -0600

    Imported Custom Highlight directive

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { BooksListComponent } from './books/books-list/books-list.component';
 import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from './shared/highlight.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, BooksListComponent],
+  declarations: [ AppComponent, BooksListComponent, HighlightDirective],
   bootstrap:    [ AppComponent]
 })
 
