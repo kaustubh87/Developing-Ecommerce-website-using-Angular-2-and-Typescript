165ab68cfa701933e9e5a2b4dc7c51120ce99e59
kvin1087 <kaustubh87@gmail.com>
2018-01-17 12:00:27 -0600

    Added properties to the book

--- app/books/books-list/books-list.component.ts
+++ app/books/books-list/books-list.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
 })
 
 export class BooksListComponent {
+    imageWidth: number = 100;
+    bookAuthor: string = "Tom Jones";
+    bookTitle: string = "War and Peace 2";
+    bookPrice: string = "$29.95";
+    bookDescription: string = "Book of fiction";
+    bookReviews: number = 15;
+    bookImageUrl:string = "app/assets/images/656.jpg"
 
 }
