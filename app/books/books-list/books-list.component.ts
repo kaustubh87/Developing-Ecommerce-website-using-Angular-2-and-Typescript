802b437060a115153392236c455c6a56bb14bdbb
kvin1087 <kaustubh87@gmail.com>
2018-01-17 11:53:17 -0600

    Created a new component

---
+++ app/books/books-list/books-list.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'bs-books-list',
+    templateUrl: '../books-list/books-list.component.html'
+})
+
+export class BooksListComponent {
+
+}
