578d4ffef92297a051cbba1ef15bc5569c729eda
kvin1087 <kaustubh87@gmail.com>
2018-01-17 13:50:34 -0600

    Imported Forms Module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { BooksListComponent } from './books/books-list/books-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, BooksListComponent],
   bootstrap:    [ AppComponent]
 })
