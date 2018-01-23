4fb8f6cd28f8fb4f315493781eb60b5f36d3f69f
kvin1087 <kaustubh87@gmail.com>
2018-01-22 20:29:11 -0600

    Removed previous properties

--- app/books/book.js
+++ app/books/book.js
@@ -1,2 +1,18 @@
+/*export interface IBook{
+    id: string;
+    name: string;
+    productCode: string;
+    releaseDate: Date;
+    description: string;
+    author: string;
+    genre: string;
+    specifications: string;
+    inStock: boolean;
+    price: number;
+    starRating: number;
+    imageUrl: string;
+
+}
+*/
 "use strict";
 //# sourceMappingURL=book.js.map
