b8bc91a6014f75b3e9a9fad6c5d07985981512a7
kvin1087 <kaustubh87@gmail.com>
2018-01-23 21:21:01 -0600

    Used previous interface properties

--- app/books/book.js
+++ app/books/book.js
@@ -1,18 +1,17 @@
-/*export interface IBook{
-    id: string;
-    name: string;
-    productCode: string;
-    releaseDate: Date;
-    description: string;
-    author: string;
-    genre: string;
-    specifications: string;
-    inStock: boolean;
-    price: number;
-    starRating: number;
-    imageUrl: string;
-
-}
-*/
 "use strict";
+/*
+export interface IBook{
+    bookTitle: string;
+    bookAuthor: string;
+    productCode?:string;
+    publishedOn: Date;
+    bookDescription: string;
+    genre?:string;
+    specifications?:string;
+    inStock: string;
+    bookPrice: number;
+    bookReviews: number;
+    bookImageUrl: string;
+}
+*/ 
 //# sourceMappingURL=book.js.map
